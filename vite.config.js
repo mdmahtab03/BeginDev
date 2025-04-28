82b58b824c0fde223d0d974383d72300c99f61e2
mahtab900 <mdmahtab9006@gmail.com>
2025-04-29 01:16:09 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/mdmahtab03/BeginDev.git',
+  base: '/BeginDev/',
   plugins: [react(),tailwindcss(),],
 })
